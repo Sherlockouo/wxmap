d6f2dfd7b9e5d5a675b46f057caba3bd746103e6
wdf <myqq12345@outlook.com>
2021-02-19 16:23:54 +0800

    add register and login

--- pages/man/man.js
+++ pages/man/man.js
@@ -49,6 +49,65 @@ Page({
     });
   },
 
+  wxlogin: function(e){
+    if(e.detail.userInfo){
+      var that = this
+      wx.login({
+        success: res => {
+          // 获取到用户的 code 之后：res.code
+          console.log("用户的code:" + res.code);
+          // 可以传给后台，再经过解析获取用户的 openid
+          // 或者可以直接使用微信的提供的接口直接获取 openid ，方法如下：
+          wx.request({
+              // 自行补上自己的 APPID 和 SECRET
+              url: 'https://storymap.sherlockouo.com/user/login',
+              method: "POST",
+              header: {
+                'content-type': 'application/x-www-form-urlencoded' // 默认值
+              },
+              data:{
+                wxcode:res.code
+              },
+              success: res => { 
+                // 获取到用户的信息了，打印到控制台上看下
+                  console.log("用户的信息如下：");
+                  console.log(e.detail.userInfo);
+                  //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
+                  that.setData({
+                    isHide: false,
+                    userInfo: e.detail.userInfo
+                  });
+                  // 获取到用户的 openid
+                  if(res.code==0){
+                  console.log("用户的token " + res.token);
+                  }else{
+                    console.log("msg ",res.data.msg);
+                  }
+              },
+              fail: res=>{
+                  console.log("shit failed");
+              }
+          });
+        }
+      });
+    
+  } else {
+    //用户按了拒绝按钮
+    wx.showModal({
+      title: '警告',
+      content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+      showCancel: false,
+      confirmText: '返回授权',
+      success: function(res) {
+        // 用户没有授权成功，不需要改变 isHide 的值
+        if (res.confirm) {
+          console.log('用户点击了“返回授权”');
+        }
+      }
+    });
+  }
+  },
+
   bindGetUserInfo: function(e) {
     if (e.detail.userInfo) {
       //用户按了允许授权按钮
@@ -81,6 +140,14 @@ Page({
                     success: res => {
                         // 获取到用户的 openid
                         console.log("用户的openid:" + res.data);
+                         // 获取到用户的信息了，打印到控制台上看下
+                        console.log("用户的信息如下：");
+                        console.log(e.detail.userInfo);
+                        //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
+                        that.setData({
+                          isHide: false,
+                          userInfo: e.detail.userInfo
+                        });
                     },
                     fail: res=>{
                         console.log("shit failed");
@@ -97,14 +164,7 @@ Page({
         
       })
      
-      // 获取到用户的信息了，打印到控制台上看下
-      console.log("用户的信息如下：");
-      console.log(e.detail.userInfo);
-      //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
-      that.setData({
-        isHide: false,
-        userInfo: e.detail.userInfo
-      });
+     
     } else {
       //用户按了拒绝按钮
       wx.showModal({
@@ -120,6 +180,14 @@ Page({
         }
       });
     }
+  },
+  /**
+  * 自定义函数
+  */
+  myinfopage:function(){
+    wx.navigateTo({
+      url: '/pages/myinfo/myinfo',
+    })
   },
 
   /**
